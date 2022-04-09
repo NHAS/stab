006eae578cdd95dd755be446c12a3db6474beb80
NHAS <jordanatararimu@gmail.com>
2022-04-09 15:56:08 +1200

    Addy boi

---
+++ shellcode.go
@@ -0,0 +1,14 @@
+package main
+
+type Shellcode struct {
+	Instruct         []byte
+	EntryPointOffset uintptr
+	HandleOffset     uintptr
+}
+
+func (s *Shellcode) Resolve(Handle, EntryPoint uintptr) []byte {
+	Write(Handle, s.Instruct[code.HandleOffset:])
+	Write(EntryPoint, s.Instruct[code.EntryPointOffset:])
+
+	return s.Instruct
+}
