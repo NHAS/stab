84f7d33cfd22e3ddd06f776cdad4d51f4033b084
NHAS <jordanatararimu@gmail.com>
2022-04-09 14:22:15 +1200

    Add constants

---
+++ constants_386.go
@@ -0,0 +1,5 @@
+//go:build windows && 386
+
+package main
+
+const IMAGE_ORDINAL = uint32(0x80000000)
---
+++ constants_amd64.go
@@ -0,0 +1,5 @@
+//go:build windows && amd64
+
+package main
+
+const IMAGE_ORDINAL = uint64(0x8000000000000000)
